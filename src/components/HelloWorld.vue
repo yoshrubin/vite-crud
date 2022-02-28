63576c4d8e2b27ea9cec564e7d3d5f7b7d1694b8
Yoshi Rubin <yoshrubin@gmail.com>
2022-02-28 19:19:22 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div id="app">
+    <p>You clicked {{ count }} times</p>
+    <button @click="count++">Click me</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
